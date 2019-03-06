6d43ef8638c738e3d94a59f56086c746ffc8fb22
chengscott <60510scott@gmail.com>
2019-03-06 22:57:44 +0800

    fix env

--- gym_thegame/envs/client.py
+++ gym_thegame/envs/client.py
@@ -1,4 +1,5 @@
 from thegame import HeadlessClient
+from gym import logger
 import numpy as np
 
 
@@ -18,83 +19,61 @@ class Client(HeadlessClient):
     self.pipe_obv_reward.send((self.observation, self.reward))
     # receive actions
     actions = self.pipe_actions.recv()
-    acc_dir, shoot_dir, ability_type = actions
-    print("recv actions:", actions)
+    logger.info('recv actions', actions)
     # perform actions
+    acc_dir, shoot_dir, ability_type = actions
     self.accelerate(acc_dir)
     self.shoot(shoot_dir)
     self.level_up(ability_type)
 
   def _to_state(self, hero, heroes, polygons, bullets):
-    """
-    green
-    self hero, bullets:
-    rgb(16, 79, 15) ~ rgb(119, 226, 118)
+    def interp(color_range, ratio):
+      return (int(end - (end - start) * ratio)
+              for start, end in zip(*color_range))
 
-    blue
-    3 Polygon:
-    rgb(33, 47, 104) ~ rgb(96, 115, 196)
-    4 Polygon:
-    rgb(33, 23, 84) ~ rgb(112, 98, 188)
-    5 Polygon:
-    rgb(23, 63, 89) ~ rgb(89, 148, 186)
-
-    red
-    other heros, bullets:
-    rgb(114, 11, 11) ~ rgb(239, 103, 103)
-    """
-
-    hero_color_range = [[16, 119], [79, 226], [15, 118]]
-    polygon_color_range = [[], [], [], [[33, 96], [47, 115], [104, 196]],
-                           [[33, 112], [23, 98], [84, 188]],
-                           [[23, 89], [63, 148], [89, 186]]]
-    other_color_range = [[[114, 239], [11, 103], [11, 103]]]
-
-    def interpolate(color_range, health, max_health):
-      min_value = color_range[0]
-      max_value = color_range[1]
-      return int(max_value - (max_value - min_value) * health / max_health)
-
-    def draw_value(arr, value, center, radius, layer, hero_pos):
-      x, y = center
-      hx, hy = hero_pos
+    def draw(state, obj, color_range):
+      """draw `obj` relative to current hero position"""
+      color = interp(color_range, obj.health / obj.max_health)
+      hx, hy = hero.position
+      x, y = obj.position
       x, y, hx, hy = x / 10, y / 10, hx / 10, hy / 10
+      radius = obj.radius
       x += self.width / 2 - hx
       y += self.height / 2 - hy
-      sx = int(max(0, x - radius))
-      sy = int(max(0, y - radius))
-      ex = int(min(self.width, x + radius))
-      ey = int(min(self.height, y + radius))
-      for i in range(sx, ex):
-        for j in range(sy, ey):
-          if (i - x)**2 + (j - y)**2 <= radius**2:
-            arr[i, j, layer] = value
-      return arr
+      lx, rx = int(max(0, x - radius)), int(min(self.width, x + radius))
+      ly, ry = int(max(0, y - radius)), int(min(self.height, y + radius))
+      for channel, value in enumerate(color):
+        for i in range(lx, rx):
+          for j in range(ly, ry):
+            if (i - x)**2 + (j - y)**2 <= radius**2:
+              state[i, j, channel] = value
+      return state
+
+    #
+    # color range: (start rgb, end rgb)
+    #
+    # hero, own bullets: green
+    hero_color = ((16, 79, 15), (119, 226, 118))
+    # polygons: blue
+    polygon_color = {
+        3: ((33, 47, 104), (96, 115, 196)),
+        4: ((33, 23, 84), (112, 98, 188)),
+        5: ((23, 63, 89), (89, 148, 186)),
+    }
+    # heroes, other bullets: red
+    other_color = ((114, 11, 11), (239, 103, 103))
 
     state = np.zeros((self.width, self.height, 3))
-    for i in range(3):
-      state = draw_value(
-          state, interpolate(hero_color_range[i], hero.health,
-                             hero.max_health), hero.position, hero.radius, i,
-          hero.position)
-      for p in polygons:
-        state = draw_value(
-            state,
-            interpolate(polygon_color_range[p.edges][i], p.health,
-                        p.max_health), p.position, p.radius, i, hero.position)
-      for b in bullets:
-        if b.id == hero.id:
-          state = draw_value(
-              state, interpolate(hero_color_range[i], b.health, b.max_health),
-              b.position, b.radius, i, hero.position)
-        else:
-          state = draw_value(
-              state, interpolate(other_color_range[i], b.health, b.max_health),
-              b.position, b.radius, i, hero.position)
-      for h in heroes:
-        state = draw_value(
-            state, interpolate(other_color_range[i], h.health, h.max_health),
-            h.position, h.radius, i, hero.position)
+    draw(state, hero, hero_color)
+    for polygon in polygons:
+      draw(state, polygon, polygon_color[polygon.edges])
+    for bullet in bullets:
+      if bullet.id == hero.id:
+        draw(state, bullet, hero_color)
+      else:
+        draw(state, bullet, other_color)
+    for hero_ in heroes:
+      draw(state, hero_, other_color)
 
     return state
 
--- gym_thegame/envs/thegame_env.py
+++ gym_thegame/envs/thegame_env.py
@@ -1,30 +1,58 @@
 import gym
-from gym import error, spaces
+from gym import logger, spaces
 from gym.utils import seeding
 from gym_thegame.envs.server import Server
 from gym_thegame.envs.client import Client
-from time import sleep
+import math
 import multiprocessing
+from time import sleep
+
 
 class ThegameEnv(gym.Env):
-  WIDTH, HEIGHT = 200, 100
+  """thegame environment
+  """
+  WIDTH, HEIGHT = 160, 160
+  TOTAL_STEPS = 10000
   metadata = {
       'render.modes': ['human', 'rgb_array'],
-      'video.frames_per_second': 30,
       'video.res_w': WIDTH,
       'video.res_h': HEIGHT,
   }
 
   def __init__(self, server='./thegame-server', port=50051, name='gym'):
+    self.server_bin, self.port = server, port
+    self.name = name
+    self.server, self.client = None, None
     self.observation_space = spaces.Box(
         shape=(ThegameEnv.WIDTH, ThegameEnv.HEIGHT, 3), low=0, high=255)
     # shoot, accelerate, ability
     self.action_space = spaces.MultiDiscrete([12, 12, 8])
 
+  def seed(self, seed=None):
+    self.np_random, seed = seeding.np_random(seed)
+    return [seed]
+
+  def step(self, actions):
+    def convert_to_radians(actions):
+      acc_dir, shoot_dir, ability_type = actions
+      acc_dir = acc_dir / 12 * 2 * math.pi
+      shoot_dir = shoot_dir / 12 * 2 * math.pi
+      return acc_dir, shoot_dir, ability_type
+
+    actions = convert_to_radians(actions)
+    self.pipe_actions[0].send(actions)
+    self.server.sync()
+    obv, reward = self.pipe_obv_reward[1].recv()
+    self.counter += 1
+    done = self.counter >= ThegameEnv.TOTAL_STEPS
+    return obv, reward, done, {}
+
+  def reset(self):
+    self.close()
     # thegame server
-    self.server = Server(server, port)
+    self.server = Server(self.server_bin, self.port)
     self.server.start()
-    sleep(0.1)
+    sleep(2)
 
     # thegame client
     self.pipe_obv_reward = multiprocessing.Pipe()
@@ -32,24 +60,17 @@ class ThegameEnv(gym.Env):
     self.client = multiprocessing.Process(
         target=Client.main,
         args=(
-            'localhost:{}'.format(port),
-            name,
+            'localhost:{}'.format(self.port),
+            self.name,
             ThegameEnv.WIDTH,
             ThegameEnv.HEIGHT,
             self.pipe_actions[1],
             self.pipe_obv_reward[0],
         ))
     self.client.start()
-    sleep(0.1)
+    sleep(2)
 
-  def step(self, actions):
-    self.pipe_actions[0].send(actions)
-    self.server.sync()
-    obv, reward = self.pipe_obv_reward[1].recv()
-    # TODO: done
-    return obv, [reward], [done], []
-
-  def reset(self):
+    self.counter = 0
     self.server.sync()
     obv, _ = self.pipe_obv_reward[1].recv()
     return obv
@@ -58,5 +79,7 @@ class ThegameEnv(gym.Env):
     pass
 
   def close(self):
-    self.client.terminate()
-    self.server.terminate()
+    if self.client:
+      self.client.terminate()
+    if self.server:
+      self.server.terminate()
